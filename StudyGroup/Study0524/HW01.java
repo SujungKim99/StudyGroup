0a9e7dc323622a39850ea28dacc006ef538bc0b2
sujungkim99 <161662701+SujungKim99@users.noreply.github.com>
2024-05-24 17:26:56 +0900

    test

---
+++ StudyGroup/Study0524/HW01.java
@@ -0,0 +1,7 @@
+package StudyGroup.Study0524;
+public class HW01 {
+    public static void main(String[] args) {
+        //문제 1. Students 클래스에 이름,나이,지역 필드를 생성하시오.
+        //문제 2. 생성한 필드를 호출하여 각 필드에 Alex,26,대전을 입력하시오
+    }
+}
